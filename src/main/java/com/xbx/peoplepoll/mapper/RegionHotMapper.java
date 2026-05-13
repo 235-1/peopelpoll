e8ac4e1ff6f44b1946e8bd1d7ab941ef71f42301
Tom X <419921393@qq.com>
2026-05-13 23:54:16 +0800

    热点图完成

---
+++ src/main/java/com/xbx/peoplepoll/mapper/RegionHotMapper.java
@@ -0,0 +1,6 @@
+package com.xbx.peoplepoll.mapper;/**
+ * @author X
+ * @date 2026/5/13 22:23
+ */
+public class RegionHotMapper {
+}
---
+++ src/main/java/com/xbx/peoplepoll/pojo/RegionHot.java
@@ -0,0 +1,6 @@
+package com.xbx.peoplepoll.pojo;/**
+ * @author X
+ * @date 2026/5/13 22:06
+ */
+public class RegionHot {
+}
